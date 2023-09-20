1d726f0a87218bfc01a26c5c0e6cc398662aa7a6
nikhilmaheshwari01 <nikhilme1234567@gmail.com>
2023-09-21 00:55:57 +0530

    first commit

---
+++ basic.java
@@ -0,0 +1,18 @@
+public class basic {
+    public static void main(String[] args) {
+        byte ag = 20;
+        int phone = 1234567890;
+        long phone1 = 12345678900L;
+        float pi = 3.14F;
+        String name = "Amannn";
+        int age = 30;
+        String neighbour = "monu";
+        String friend = neighbour;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(friend);
+        char letter = 'n';
+        boolean yes = true;
+        System.out.println(name.length());
+    }
+}
---
+++ myfirst.java
@@ -0,0 +1,26 @@
+
+public class myfirst {
+    public static void main(String[] args) {
+        byte ag = 20;
+        int phone = 1234567890;
+        long phone1 = 12345678900L;
+        float pi = 3.14F;
+        String name = "Amannn";
+        int age = 30;
+        String neighbour = "monu";
+        String friend = neighbour;
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(friend);
+        char letter = 'n';
+        boolean yes = true;
+        System.out.println(name.length());
+        System.out.println(ag);
+        System.out.println(phone);
+        System.out.println(phone1);
+        System.out.println(pi);
+        System.out.println(letter);
+        System.out.println(yes);
+
+    }
+}
