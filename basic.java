91bc43b25f0b752141b5d5a0c6ee1cfcfd743981
nikhilmaheshwari01 <nikhilme1234567@gmail.com>
2023-09-21 00:59:54 +0530

    first commit

--- basic.java
+++ basic.java
@@ -3,13 +3,15 @@ public class basic {
         byte ag = 20;
         int phone = 1234567890;
         long phone1 = 12345678900L;
+
         float pi = 3.14F;
         String name = "Amannn";
         int age = 30;
         String neighbour = "monu";
         String friend = neighbour;
         System.out.println(age);
-        System.out.println(name);
+
+        System.out.println(n);
         System.out.println(friend);
         char letter = 'n';
         boolean yes = true;
